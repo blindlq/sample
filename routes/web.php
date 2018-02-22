094890b266ff447d3ac7b5d588c2a5746f75fdbb
blindlq <735560449@qq.com>
2018-02-22 10:09:47 +0000

    关注动作

--- routes/web.php
+++ routes/web.php
@@ -48,5 +48,7 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('passw
 Route::get('users/{user}/followings','UsersController@followings')->name('users.followings');
 Route::get('users/{user}/followers','UsersController@followers')->name('users.followers');
 
+//关注用户和取消用户
+Route::post('users/followers/{user}','FollowersController@store')->name('followers.store');
+Route::delete('/users/followers/{user}', 'FollowersController@destroy')->name('followers.destroy');
 
-Route::post('users/followers/{user}',
